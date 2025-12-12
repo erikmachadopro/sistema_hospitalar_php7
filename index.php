8d6ff3478ee86d1728d917055db17c4624328665
erikmachadopro <erikmachadopro@gmail.com>
2025-12-12 15:37:13 -0300

    integrando bootstrap e css

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="ept-br">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Sistema Hospitalar</title>
+
+        <!-- INTEGRANDO BOOTSTRAP -->
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@3.3.7/dist/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+
+        <!-- INTEGRANDO CSS -->
+        <link rel="stylesheet" href="css/login.css">
+    </head>
+    <body>
+        
+    </body>
+</html>
